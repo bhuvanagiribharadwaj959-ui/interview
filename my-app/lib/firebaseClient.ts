8af7bc340f1d69b06aa105aa64267f6bb44faf01
bharadwaj <bhhuvanagiribharadwaj959@gmail.com>
2026-07-15 23:15:49 +0530

    chore: extract firebase config to environment variables

--- my-app/lib/firebaseClient.ts
+++ my-app/lib/firebaseClient.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8yJKbmeI4uF3DDfdOxlOYQGvBGIo2kVI",
-  authDomain: "interview-e752e.firebaseapp.com",
-  projectId: "interview-e752e",
-  storageBucket: "interview-e752e.firebasestorage.app",
-  messagingSenderId: "1004784786750",
-  appId: "1:1004784786750:web:9c0dfa4846f3a49f2ee06f",
-  measurementId: "G-VCPXFSWPQ2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
